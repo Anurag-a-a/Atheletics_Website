edc7145fd883d54b531bbab2aaac88927e43fa71
Nimmikrishna Babu <nbabu@stevens.edu>
2023-04-20 18:24:52 -0400

    membership plan details page
    Also made some design changes in amenities, landingPage, main. started joinNow

--- final_main/routes/index.js
+++ final_main/routes/index.js
@@ -2,6 +2,9 @@
 import userRoutes from './users.js';
 const constructorMethod = (app) => {
     app.use('/', userRoutes);
+    app.use('/amenities', userRoutes);
+    app.use('/membershipdetails', userRoutes);
+    app.use('/joinnow', userRoutes);
     app.use('*', (req, res) => {
       res.status(404).json({ error: '404 Error: Not found' });
     });
--- final_main/routes/users.js
+++ final_main/routes/users.js
@@ -6,11 +6,19 @@ const router = Router();
 router.route('/').get(async (req, res) => {
     //code here for Getting the main page of the gym
     return res.render('landingPage',{title: "Gym Brat"});
-  });
+});
 router.route('/amenities').get(async (req, res) => {
     //code here for Getting the main page of the gym
     return res.render('amenities',{title: "Gym Brat"});
-  });
+});
+router.route('/membershipdetails').get(async (req, res) => {
+    //code here for Getting the main page of the gym
+    return res.render('membershipPlan',{title: "Gym Brat"});
+});
+router.route('/joinnow').get(async (req, res) => {
+    //code here for Getting the main page of the gym
+    return res.render('membershipPlan',{title: "Gym Brat"});
+});
 
 
 export default router;
