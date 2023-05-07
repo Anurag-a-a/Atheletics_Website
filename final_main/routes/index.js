fa20f3f4dceb2414d909c7eee41e8a18bb949872
guocong <congguocn@outlook.com>
2023-05-06 23:59:10 -0400

    delete the duplicate information under the routes/index.js

--- final_main/routes/index.js
+++ final_main/routes/index.js
@@ -19,31 +19,13 @@ const constructorMethod = (app) => {
   app.use('/logout', userRoutes);
   app.use('/location', gymRoutes);
   app.use('/membershipplandetails', gymRoutes);
-  app.use('/reviews', reviewRoutes);
+  app.use('/myReviews', reviewRoutes);
   app.use('/myAppointments', appointmentRoutes);
   app.use('/classandevent', classandeventRoutes);
+  app.use('/allreviews', gymreviewsRoutes);
   app.use('*', (req, res) => {
     res.status(404).json({ error: '404 Error: Not found' });
   });
 };
-
-    app.use(loggingMiddleware);
-    app.use('/', userRoutes);
-    app.use('/amenities', userRoutes);
-    app.use('/membershipPlans', userRoutes);
-    app.use('/joinnow', userRoutes);
-    app.use('/signin', userRoutes);
-    app.use('/protectedUserHomePage', userRoutes);
-    app.use('/userProfile', userRoutes);
-    app.use('/updateplan', userRoutes);
-    app.use('/logout', userRoutes);
-    app.use('/locations', gymRoutes);
-    app.use('/membershipplandetails',gymRoutes);
-    app.use('/reviews', reviewRoutes);
-    app.use('/myAppointments', appointmentRoutes);
-    app.use('*', (req, res) => {
-      res.status(404).json({ error: '404 Error: Not found' });
-    });
-  };
   
 export default constructorMethod;
